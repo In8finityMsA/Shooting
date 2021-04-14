4259de4c00dcdf90a2ac3c8bc3afbe2e77f147a9
Sudnik Oleg <sudnik@holodilnik.ru>
2021-04-14 11:54:41 +0300

    Initial commit

---
+++ Shooting/Shooting/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shooting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const double minSizeFactor = 2.5;
+        public class Radius
+        {
+            private int? radiusInner;
+            private int? radiusOuter;
+            public int? RadiusInner 
+            { 
+                get => radiusInner;
+                set
+                {
+                    if (value < 10)
+                    {
+                        throw new ArgumentException();
+                    }
+                    radiusInner = value;
+                }
+            }
+                
+            public int? RadiusOuter
+            {
+                get => radiusOuter;
+                set
+                {
+                    if (value < 10 || value < radiusInner)
+                    {
+                        throw new ArgumentException();
+                    }
+                    radiusOuter = value;
+                }
+            }
+        }
+
+        public Radius radius = new Radius();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = radius;
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (radius.RadiusInner > 10 && radius.RadiusOuter > 10 && radius.RadiusOuter > radius.RadiusInner)
+            {
+                //radius.RadiusInner = 60;
+                //radius.RadiusOuter = 120;
+                this.Content = new ShootingPage((int)radius.RadiusInner, (int)radius.RadiusOuter);
+                this.MinHeight = (int) radius.RadiusOuter * minSizeFactor;
+                this.MinWidth = (int) radius.RadiusOuter * minSizeFactor + 200;
+            }
+            else 
+            {
+                MessageBox.Show("Radius must be greater than 10 and RadiusOuter greater than RadiusInner");
+            }
+        }
+
+        private void TextBox_Error(object sender, ValidationErrorEventArgs e)
+        {
+            //MessageBox.Show(e.Error.ErrorContent.ToString());
+        }
+
+        private void tbxHinted1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtHint1.Visibility = Visibility.Visible;
+            if (tbxEnterInnerRadius.Text.Length > 0)
+            {
+                txtHint1.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void tbxHinted2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtHint2.Visibility = Visibility.Visible;
+            if (tbxEnterOuterRadius.Text.Length > 0)
+            {
+                txtHint2.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ Shooting/Shooting/ShootingPage.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shooting
+{
+    /// <summary>
+    /// Логика взаимодействия для Shooting.xaml
+    /// </summary>
+    public partial class ShootingPage : Page
+    {
+        private int radiusInner;
+        private int radiusOuter;
+
+        private int hits = 0;
+        private int missed = 0;
+        private int leftShots = 5;
+
+        private GeometryGroup shotsMissedGroup = new GeometryGroup();
+        private GeometryGroup shotsHitGroup = new GeometryGroup();
+        private Path pathTarget;
+        private double bulletRadius;
+        private const double TargetToBulletRatio = 12;
+        
+
+        public ShootingPage(int radiusInner, int radiusOuter)
+        {
+            InitializeComponent();
+            this.radiusInner = radiusInner;
+            this.radiusOuter = radiusOuter;
+            bulletRadius = (radiusOuter - radiusInner) / TargetToBulletRatio;
+
+            shotsHitGroup.FillRule = FillRule.Nonzero;
+            CombinedGeometry combinedGeometry = new CombinedGeometry();
+            combinedGeometry.GeometryCombineMode = GeometryCombineMode.Exclude;
+            combinedGeometry.Geometry1 = CreateTarget();
+            combinedGeometry.Geometry2 = shotsHitGroup;
+
+            pathTarget = new Path();
+            pathTarget.Data = combinedGeometry;
+            pathTarget.Stroke = new SolidColorBrush(Colors.Red);
+            pathTarget.Fill = new SolidColorBrush(Colors.Orange);
+            pathTarget.MouseDown += Path_MouseDown;
+            
+            gridView.Children.Add(pathTarget);                      
+            gridView.IsHitTestVisible = true;
+            gridView.MinWidth = radiusOuter * MainWindow.minSizeFactor;
+            gridView.MinHeight = radiusOuter * MainWindow.minSizeFactor;
+            view.MinHeight = radiusOuter * MainWindow.minSizeFactor;
+            view.MinWidth = radiusOuter * MainWindow.minSizeFactor;
+            
+
+            shotsMissedGroup.FillRule = FillRule.Nonzero;
+            pathMissed.Data = shotsMissedGroup;
+            pathMissed.Fill = new SolidColorBrush(Colors.Red);
+
+            shotsMissedGroup.Children.Add(new EllipseGeometry(new Point(0, 0), 1, 1)); //Debug
+        }
+
+        private void GridView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (leftShots > 0)
+            {
+                SoundPlayer player = new SoundPlayer(@"Resources/pock.wav");
+                player.Load();
+                player.Play();
+                Point mouse = Mouse.GetPosition(pathMissed);
+                EllipseGeometry ellipse = new EllipseGeometry(mouse, bulletRadius, bulletRadius);
+                shotsMissedGroup.Children.Add(ellipse);
+
+                lblNumLeft.Content = --leftShots;
+                lblNumMissed.Content = ++missed;
+            }
+            e.Handled = true;
+        }
+
+        private void Path_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (leftShots > 0)
+            {
+                SoundPlayer player = new SoundPlayer(@"Resources/bang.wav");
+                player.Load();
+                player.Play();
+                Point mouse = Mouse.GetPosition(pathTarget);
+                EllipseGeometry ellipse = new EllipseGeometry(mouse, bulletRadius, bulletRadius);
+                shotsHitGroup.Children.Add(ellipse);
+
+                lblNumLeft.Content = --leftShots;
+                lblNumHits.Content = ++hits;
+            }
+            e.Handled = true;
+        }
+
+        private PathGeometry CreateTarget()
+        {
+
+            PathGeometry path = new PathGeometry();
+            path.FillRule = FillRule.Nonzero;
+
+            PathFigure pf = new PathFigure();
+            pf.StartPoint = new Point(0, -radiusInner);
+            pf.IsClosed = true;
+            pf.Segments.Add(new LineSegment(new Point(0, -radiusOuter), true));
+            pf.Segments.Add(new ArcSegment(new Point(radiusOuter, 0), new Size(radiusOuter, radiusOuter), 0.0, false, SweepDirection.Clockwise ,true));
+            pf.Segments.Add(new LineSegment(new Point(radiusInner, 0), true));
+            pf.Segments.Add(new ArcSegment(new Point(0, -radiusInner), new Size(radiusInner, radiusInner), 0.0, false, SweepDirection.Counterclockwise, true));
+            path.Figures.Add(pf);
+
+            PathFigure pf2 = new PathFigure();
+            pf2.StartPoint = new Point(0, radiusInner);
+            pf2.IsClosed = true;
+            pf2.Segments.Add(new LineSegment(new Point(0, radiusOuter), true));
+            pf2.Segments.Add(new ArcSegment(new Point(-radiusOuter, 0), new Size(radiusOuter, radiusOuter), 0.0, false, SweepDirection.Clockwise, true));
+            pf2.Segments.Add(new LineSegment(new Point(-radiusInner, 0), true));
+            pf2.Segments.Add(new ArcSegment(new Point(0, radiusInner), new Size(radiusInner, radiusInner), 0.0, false, SweepDirection.Counterclockwise, true));
+            path.Figures.Add(pf2);
+
+            return path;
+        }
+
+        private void gridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            pathTarget.RenderTransform = new TranslateTransform(gridView.ActualWidth / 2, gridView.ActualHeight / 2);
+            pathMissed.Width = gridView.ActualWidth;
+            pathMissed.Height = gridView.ActualHeight;
+            
+        }
+    }
+}
